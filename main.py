1ed6185514a8165d810c60be6e64d3b7c5d0db1f
xotiic4201 <xotiicglizzy@gmail.com>
2026-03-03 11:56:21 -0800

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://gamef-swart.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
